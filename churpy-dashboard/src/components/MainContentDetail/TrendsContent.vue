bb983de9936ac2f02374911d99fae56457757a2e
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-07-05 08:44:11 +0300

    TrendsContent - add footer section to the trends content
    contains a button link to view all sales trends

--- churpy-dashboard/src/components/MainContentDetail/TrendsContent.vue
+++ churpy-dashboard/src/components/MainContentDetail/TrendsContent.vue
@@ -18,11 +18,35 @@
       ></span>
     </p>
     <TrendsGraph />
+    <div class="flex flex-col mt-3 mb-3 gap-y-1">
+      <div class="trend-footer-title flex justify-start items-center gap-x-3">
+        <p>Your sales trend</p>
+        <i class="fa-solid text-sm fa-angle-right"></i>
+      </div>
+      <div class="flex flex-wrap gap-y-1 justify-between items-center">
+        <small>Gathered from periodical sales operations</small>
+        <button class="text-white px-2 py-1 rounded">
+          View Sales <i class="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
   .trends-title-highlight {
     color: #4ed582;
+  }
+
+  small {
+    color: #a1a6ad;
+  }
+
+  .trend-footer-title {
+    color: #1f3162;
+  }
+
+  button {
+    background-color: #4ed582;
   }
 </style>
