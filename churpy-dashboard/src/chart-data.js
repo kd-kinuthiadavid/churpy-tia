52de806b63a2ba7d1964d10aea85d8994e05401e
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-07-05 10:23:01 +0300

    charts - replace existing chart with a chart.js line-chart

---
+++ churpy-dashboard/src/chart-data.js
@@ -0,0 +1,33 @@
+const chartData = {
+  type: "line",
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    datasets: [
+      {
+        label: "Trends",
+        data: [
+          500.166, 120.081, 3.003, 300.323, 954.792, 285.886, 43.662, 51.514,
+        ],
+        backgroundColor: "rgba(71, 183,132,.5)",
+        borderColor: "#47b784",
+        borderWidth: 3,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    lineTension: 1,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            padding: 25,
+          },
+        },
+      ],
+    },
+  },
+};
+
+export default chartData;
--- churpy-dashboard/src/components/MainContentDetail/TrendsGraph.vue
+++ churpy-dashboard/src/components/MainContentDetail/TrendsGraph.vue
@@ -1,76 +1,24 @@
-// @TODO: Make the chart dynamic
 <script>
-  import { Bar } from "vue-chartjs";
-  import {
-    Chart as ChartJS,
-    Title,
-    Tooltip,
-    Legend,
-    BarElement,
-    CategoryScale,
-    LinearScale,
-  } from "chart.js";
-
-  ChartJS.register(
-    Title,
-    Tooltip,
-    Legend,
-    BarElement,
-    CategoryScale,
-    LinearScale
-  );
+  import Chart from "chart.js";
+  import chartData from "../../chart-data.js";
 
   export default {
-    components: { Bar },
-    props: {
-      chartId: {
-        type: String,
-        default: "bar-chart",
-      },
-      datasetIdKey: {
-        type: String,
-        default: "label",
-      },
-      width: {
-        type: Number,
-        default: 400,
-      },
-      cssClasses: {
-        default: "",
-        type: String,
-      },
-      styles: {
-        type: Object,
-        default: () => {},
-      },
-      plugins: {
-        type: Object,
-        default: () => {},
-      },
-    },
+    name: "TrendsChart",
+
     data() {
       return {
-        chartData: {
-          labels: ["January", "February", "March"],
-          datasets: [{ data: [40, 20, 12] }],
-        },
-        chartOptions: {
-          responsive: true,
-        },
+        chartData: chartData,
       };
+    },
+    mounted() {
+      const ctx = document.getElementById("trends-chart");
+      new Chart(ctx, this.chartData);
     },
   };
 </script>
 
 <template>
-  <Bar
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-  />
+  <div>
+    <canvas id="trends-chart"></canvas>
+  </div>
 </template>
