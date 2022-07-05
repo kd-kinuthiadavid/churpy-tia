22ff77fb0433e800f489948b770f9b2f433a4b54
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-07-05 08:18:08 +0300

    IconsNav - use flex to align the icons so that they match the design spec

--- churpy-dashboard/src/components/IconsNav.vue
+++ churpy-dashboard/src/components/IconsNav.vue
@@ -1,24 +1,26 @@
 <template>
   <div
-    class="bg-inherit flex flex-col justify-center gap-y-10 mx-4 my-3 w-1/12"
+    class="bg-inherit flex flex-col justify-between gap-y-10 mx-4 my-3 w-1/12"
   >
     <img
-      class="w-14 h-14 mb-40 ml-4"
+      class="h-20 w-20 mt-10 ml-4"
       src="../assets/images/Logo-icon.svg"
       alt="logo"
     />
-    <i
-      class="cursor-pointer fa-solid fa-building-columns text-3xl text-white flex justify-center"
-    ></i>
-    <i
-      class="cursor-pointer fa-solid fa-boxes-stacked text-3xl text-white flex justify-center"
-    ></i>
-    <i
-      class="cursor-pointer fa-solid fa-chart-column text-3xl text-white flex justify-center"
-    ></i>
-    <i
-      class="cursor-pointer fa-solid fa-users text-3xl text-white flex justify-center"
-    ></i>
+    <div class="flex flex-col justify-center mb-60 gap-y-10">
+      <i
+        class="cursor-pointer fa-solid fa-building-columns text-3xl text-white flex justify-center"
+      ></i>
+      <i
+        class="cursor-pointer fa-solid fa-boxes-stacked text-3xl text-white flex justify-center"
+      ></i>
+      <i
+        class="cursor-pointer fa-solid fa-chart-column text-3xl text-white flex justify-center"
+      ></i>
+      <i
+        class="cursor-pointer fa-solid fa-users text-3xl text-white flex justify-center"
+      ></i>
+    </div>
   </div>
 </template>
 
